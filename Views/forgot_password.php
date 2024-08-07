1f7df7b0ecdbfd85df1c5222264e4ef4d9a892d8
KevinChavezDv <143848179+KevinChavezDv@users.noreply.github.com>
2024-08-06 20:53:43 -0500

    Update forgot_password.php

--- Views/forgot_password.php
+++ Views/forgot_password.php
@@ -8,7 +8,8 @@
     <form action="../Public/forgot_password_action.php" method="post">
         <input type="email" name="email" placeholder="Email" required>
         <button type="submit">Enviar enlace de recuperación</button>
-        <h1>hola</h1>
+        <h1>Mi nombre es Kevin y este es mi primera vez usando GitHub </h1>
+        
     </form>
 </body>
 </html>
