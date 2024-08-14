229e19dff9d77116f71edae2a1808397b6876578
Steven Vanegas Gomez <128706059+Stely69@users.noreply.github.com>
2024-08-14 16:40:37 -0500

    Delete Public/register_action.php

--- Public/register_action.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../Controller/UserController.php';
-
-$userController = new UserController();
-$userController->register($_POST['username'], $_POST['email'], $_POST['password']);
-
-?>
