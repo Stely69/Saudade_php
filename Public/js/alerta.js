0b36a052593d5e761aaecf961512548d6318407c
Steven Vanegas Gomez <128706059+Stely69@users.noreply.github.com>
2024-08-14 16:41:00 -0500

    Delete Public/js directory

--- Public/js/alerta.js
+++
@@ -1,7 +0,0 @@
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
-
-setTimeout(function () {
-    document.getElementById('popup').style.display = 'block';
-}, 60000);
